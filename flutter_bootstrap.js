414f7f23799531ce4a56f7f9e2609de0faa6404b
Mohamed Junaid Chaudhry <bricksigma176@gmail.com>
2025-06-14 12:20:41 +0300

    Updated path

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3259601305"
+    serviceWorkerVersion: "4124272600"
   }
 });
